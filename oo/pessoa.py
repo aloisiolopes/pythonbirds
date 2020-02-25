5c34e63e73a9cae59e1a76c4a17f4308a1a9d64a
aloisiolopes <aloisiolopes19@gmail.com>
2020-02-25 18:43:28 -0300

    Criadoa atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,21 +1,24 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome #nome é respectivo a self.nome, quando não tivermos nada será None como declarado a cima
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Óla {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Aloisio')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Renzo'
-    print(p.nome)
-    print(p.idade)
+    Luiza = Pessoa(nome='Luiza')
+    Aloisio = Pessoa(Luiza, nome='Aloisio')
+    print(Pessoa.cumprimentar(Aloisio))
+    print(id(Aloisio))
+    print(Aloisio.cumprimentar())
+    print(Aloisio.nome)
+    print(Aloisio.idade)
+    for filhos in Aloisio.filhos:
+        print(filhos.nome)
+
 
 
 
